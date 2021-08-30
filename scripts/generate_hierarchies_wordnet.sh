eda20ae67a9e33e7fd068e03cd01196233c780b5
Zixian Zang <55410388+XAVILLA@users.noreply.github.com>
2021-08-29 23:52:06 -0700

    o

--- scripts/generate_hierarchies_wordnet.sh
+++ scripts/generate_hierarchies_wordnet.sh
@@ -1,13 +1,13 @@
 python -c "import nltk;nltk.download('wordnet')"
 
 # Generate WNIDs
-for dataset in CIFAR10 CIFAR100;
+for dataset in CIFAR10 CIFAR100 Office_Home;
 do
   nbdt-wnids --dataset=${dataset}
 done;
 
 # Generate and test hierarchies
-for dataset in CIFAR10 CIFAR100 TinyImagenet200;
+for dataset in CIFAR10 CIFAR100 TinyImagenet200 Office_Home;
 do
   nbdt-hierarchy --dataset=${dataset} --method=wordnet;
 done;
