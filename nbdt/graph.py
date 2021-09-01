d537a3bc0e9699e2c6d674d5ac5cdd9e22c9ad95
Zixian Zang <55410388+XAVILLA@users.noreply.github.com>
2021-08-31 17:23:43 -0700

    o

--- nbdt/graph.py
+++ nbdt/graph.py
@@ -504,7 +504,7 @@ def get_centers_from_model(model, num_classes, dataset):
 
 def get_centers_from_state_dict(state_dict):
     fc = None
-    print(type(state_dict))
+    print(state_dict.keys())
     for key in MODEL_FC_KEYS:
         if key in state_dict:
             fc = state_dict[key].squeeze()
